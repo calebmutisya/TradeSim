ae5d8109c0ca140612ad5fa4ca529691d78a02be
calebmutisya <mutisyacalebmusau@gmail.com>
2024-03-26 09:17:42 +0300

    Added Components

---
+++ client/src/layouts/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Outlet} from 'react-router-dom'
+import Navbar from './components/Navbar.js'
+import Footer from '../components/Footer.js'
+
+
+export default function Layout() {
+  return (
+    <div>
+        <Navbar/>
+        <Outlet/>
+        <Footer/>
+    </div>
+  )
+}
