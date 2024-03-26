c1ef16439d9f8754287ffe9779299237ba0ef17b
calebmutisya <mutisyacalebmusau@gmail.com>
2024-03-26 10:09:52 +0300

    imported Css

--- client/src/layouts/Layout.js
+++ client/src/layouts/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
+import '../css/App.css';
 import Navbar from '../components/Navbar.js'
 import Footer from '../components/Footer.js'
 
-
 export default function Layout() {
   return (
-    <div>
+    <div className='App'>
         <Navbar/>
         <Outlet/>
         <Footer/>
