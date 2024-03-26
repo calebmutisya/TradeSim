87a0b9e591a8e06b8aaf078dd119cea6fc4e6182
calebmutisya <mutisyacalebmusau@gmail.com>
2024-03-26 12:46:42 +0300

    Navbar designed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-
+import '../css/Navbar.css';
+import logo from '../assets/Logo.svg'
 export default function Navbar() {
   return (
     <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/markets">Markets</NavLink>
-      <NavLink to="/news">News</NavLink>
-      <NavLink to="/analysis">Analysis</NavLink>
-      <NavLink to="/guide">Guide</NavLink>
-      <NavLink to="/about">About Us</NavLink>
+      <div className='logo'><img src={logo}/>TRADESIM</div>
+      <div className='links'>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/markets">Markets</NavLink>
+        <NavLink to="/news">News</NavLink>
+        <NavLink to="/analysis">Analysis</NavLink>
+        <NavLink to="/guide">Guide</NavLink>
+        <NavLink to="/about">About Us</NavLink>
+      </div>
+      <div className='btnsect'>
+        <button>Login</button>
+        <button>Sign Up</button>
+      </div>
     </nav>
   )
 }
