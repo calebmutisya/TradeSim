1ed845bda971ef52ddd368b7cd5faa2bb5bbc284
calebmutisya <mutisyacalebmusau@gmail.com>
2024-03-26 10:08:53 +0300

    Routing Working

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/markets">Markets</NavLink>
+      <NavLink to="/news">News</NavLink>
+      <NavLink to="/analysis">Analysis</NavLink>
+      <NavLink to="/guide">Guide</NavLink>
+      <NavLink to="/about">About Us</NavLink>
+    </nav>
   )
 }
