6ccd5ba7f0c0e8868680222547c4ef2613fd6779
calebmutisya <mutisyacalebmusau@gmail.com>
2024-03-29 09:15:00 +0300

    Footer coded

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import '../css/Footer.css';
+import logo from '../assets/Logo.svg'
+import footimg from '../assets/footerimg.png'
+import twitter from '../assets/twitter.svg'
+import insta from '../assets/insta.svg'
+import linkedin from '../assets/linkedin.svg'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+        <div className='intouch'>
+          <div>Get In Touch :</div>
+          <div className='iconscont'>
+            <a><img src={twitter}/></a>
+            <a><img src={insta}/></a>
+            <a><img src={linkedin}/></a>
+          </div>
+        </div>
+        <div className='footdet'>
+          <div className='footlinks'>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/markets">Markets</NavLink>
+            <NavLink to="/news">News</NavLink>
+            <NavLink to="/analysis">Analysis</NavLink>
+            <NavLink to="/guide">Guide</NavLink>
+            <NavLink to="/about">About Us</NavLink>
+          </div>
+          <div className='footlogo'><img src={logo}/>TRADESIM</div>
+        </div>
+    </div>
   )
 }
