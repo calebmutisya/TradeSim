91cb335ed82654342690eedf8645d302f8c72c8b
calebmutisya <mutisyacalebmusau@gmail.com>
2024-04-02 13:22:34 +0300

    Added fx pair list and chart

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,7 +1,89 @@
-import React from 'react'
+import AU from '../assets/icons/AU.svg'
+import CA from '../assets/icons/CA.svg'
+import EU from '../assets/icons/EU.svg'
+import GB from '../assets/icons/GB.svg'
+import JP from '../assets/icons/JP.svg'
+import NZ from '../assets/icons/NZ.svg'
+import CH from '../assets/icons/CH.svg'
+import US from '../assets/icons/US.svg'
 
-export default function constants() {
-  return (
-    <div>constants</div>
-  )
-}
+export const fxpairs=[
+  {
+    img1: EU,
+    pair1:'EUR',
+    img2: US,
+    pair2: 'USD',
+  },
+  {
+    img1: US,
+    pair1: 'USD',
+    img2: JP,
+    pair2: 'JPY',
+  },
+  {
+    img1: GB,
+    pair1: 'GBP',
+    img2: US,
+    pair2: 'USD',
+  },
+  {
+    img1: US,
+    pair1: 'USD',
+    img2: CH,
+    pair2: 'CHF',
+  },
+  {
+    img1: AU,
+    pair1: 'AUD',
+    img2: US,
+    pair2: 'USD',
+  },
+  {
+    img1: US,
+    pair1: 'USD',
+    img2: CA,
+    pair2: 'CAD',
+  },
+  {
+    img1: NZ,
+    pair1: 'NZD',
+    img2: US,
+    pair2: 'USD',
+  },
+  {
+    img1: GB,
+    pair1: 'GBP',
+    img2: JP,
+    pair2: 'JPY',
+  },
+  {
+    img1: AU,
+    pair1: 'AUD',
+    img2: NZ,
+    pair2: 'NZD',
+  },
+  {
+    img1: EU,
+    pair1: 'EUR',
+    img2: GB,
+    pair2: 'GBP',
+  },
+  {
+    img1: CH,
+    pair1: 'CHF',
+    img2: JP,
+    pair2: 'JPY',
+  },
+  {
+    img1: GB,
+    pair1: 'GBP',
+    img2: CA,
+    pair2: 'CAD',
+  },
+  {
+    img1: AU,
+    pair1: 'AUD',
+    img2: JP,
+    pair2: 'JPY',
+  }
+]
--- client/src/pages/Markets.js
+++ client/src/pages/Markets.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../css/Markets.css'
+import { fxpairs } from '../constants/constants'
+import fire from '../assets/fire.svg'
+import watchlist from '../assets/watchlist.svg'
+import star from '../assets/star.svg'
+import TradingViewWidget from '../constants/TradingViewWidget';
+
 
 export default function Markets() {
+
+  const [selectedSymbol, setSelectedSymbol] = useState(null);
+
+  const handlePairClick = (symbol) => {
+    setSelectedSymbol(symbol);
+  }
+
   return (
-    <div>Markets</div>
+    <div className='market'>
+      <div className='chartsec'>
+        <div className='fxlist'>
+          <div className='btns'>
+            <div className='bm'><img src={fire}/>FXPAIRS</div>
+            <div className='bm'><img src={watchlist}/>WATCHLIST</div>
+          </div>
+          <div className='pairscont'>
+            {fxpairs.map((fxpair)=>(
+              <div className='fxs' key={fxpair} onClick={() => handlePairClick(`PEPPERSTONE:${fxpair.pair1}${fxpair.pair2}`)}>
+                <div><img className='star' src={star}/></div>
+                <div><img src={fxpair.img1}/></div>
+                <div>{fxpair.pair1}</div>
+                <div className='slash'>|</div>
+                <div><img src={fxpair.img2}/></div>
+                <div>{fxpair.pair2}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className='chart'>
+        {selectedSymbol && <TradingViewWidget key={selectedSymbol} symbol={selectedSymbol} />}
+        </div>
+      </div>
+      <div>
+        <div>Buy and Sell</div>
+        <div>Trades</div>
+        <div>Heat Map</div>
+      </div>
+    </div>
   )
 }
