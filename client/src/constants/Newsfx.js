a3de91a1c5fb7aabb05548a9358283ca72467a3c
calebmutisya <mutisyacalebmusau@gmail.com>
2024-04-04 14:46:10 +0300

    Coded News page and Widget

---
+++ client/src/constants/Newsfx.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useRef, memo } from 'react';
+
+function Newsfx(){
+    const container = useRef();
+
+    useEffect(
+        () => {
+        const script = document.createElement("script");
+        script.src="https://s3.tradingview.com/external-embedding/embed-widget-timeline.js";
+        script.type = "text/javascript";
+        script.async = true;
+        script.innerHTML = `
+            {
+                "feedMode": "market",
+                "market": "forex",
+                "isTransparent": false,
+                "displayMode": "regular",
+                "width": 936,
+                "height": 850,
+                "colorTheme": "dark",
+                "locale": "en"
+            }`;
+        container.current.appendChild(script);
+        },
+        []
+    );
+
+    return (
+        <div class="tradingview-widget-container" ref={container}>
+            <div class="tradingview-widget-container__widget"></div>
+            <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/markets/currencies/news/" rel="noopener nofollow" target="_blank"><span class="blue-text">Get More Detailed News !!!</span></a>
+            </div>
+        </div>
+    );
+}
+
+export default memo(Newsfx);
+
+
+
+// <!-- TradingView Widget BEGIN -->
+// <div class="tradingview-widget-container">
+//   <div class="tradingview-widget-container__widget"></div>
+//   <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+//   <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-timeline.js" async>
+//   {
+//   "feedMode": "market",
+//   "market": "forex",
+//   "isTransparent": false,
+//   "displayMode": "regular",
+//   "width": 400,
+//   "height": 550,
+//   "colorTheme": "dark",
+//   "locale": "en"
+// }
+//   </script>
+// </div>
+// <!-- TradingView Widget END -->
--- client/src/pages/News.js
+++ client/src/pages/News.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import Newsfx from '../constants/Newsfx'
+import '../css/News.css'
 
 export default function News() {
   return (
-    <div>News</div>
+    <div>
+      <header>TOP STORIES</header>
+      <p className='storydet'>Keep track of what's happening in the Currency markets with our daily news briefs – designed to be read in 20 seconds or less.</p>
+      <div className='newssec'>
+        <Newsfx/>
+      </div>
+    </div>
   )
 }
