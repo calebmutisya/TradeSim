3d08d906784d22d4904ae3cf1a4ce543aa4d05da
calebmutisya <mutisyacalebmusau@gmail.com>
2024-04-05 11:58:04 +0300

    Trade Log and Rankings partially done

--- client/src/pages/Analysis.js
+++ client/src/pages/Analysis.js
@@ -4,6 +4,7 @@ import dollar from '../assets/dollar.svg'
 import rate from '../assets/rate.svg'
 import graph from '../assets/graph.svg'
 import rank from '../assets/rank.svg'
+import profimg from '../assets/profimg.png'
 
 export default function Analysis() {
   return (
@@ -30,9 +31,87 @@ export default function Analysis() {
           <div className='cardname'>Rank</div>
         </div>
       </div>
-      <div>
-        <div>Trades Log</div>
-        <div>Rankings</div>
+      <div className='analysislog'>
+        <div className='pasttrades'>
+          <div className='contname'>Trades Log</div>
+          <div className='logdetails'>
+            <div className='date'>25 March 2024</div>
+            <div className='pastpair'>EUR/USD</div>
+            <div className='pastposition'>BUY</div>
+            <div className='takeprofit'>TP:156.676</div>
+            <div className='stoploss'>SL: 132.378</div>
+            <div className='profit'>PNL: 120.00</div>
+          </div>
+          <div className='logdetails'>
+            <div className='date'>25 March 2024</div>
+            <div className='pastpair'>EUR/USD</div>
+            <div className='pastposition'>BUY</div>
+            <div className='takeprofit'>TP:156.676</div>
+            <div className='stoploss'>SL: 132.378</div>
+            <div className='profit'>PNL: -120.00</div>
+          </div>
+          <div className='logdetails'>
+            <div className='date'>25 March 2024</div>
+            <div className='pastpair'>EUR/USD</div>
+            <div className='pastposition'>BUY</div>
+            <div className='takeprofit'>TP:156.676</div>
+            <div className='stoploss'>SL: 132.378</div>
+            <div className='profit'>PNL: -120.00</div>
+          </div>
+          <div className='logdetails'>
+            <div className='date'>25 March 2024</div>
+            <div className='pastpair'>EUR/USD</div>
+            <div className='pastposition'>BUY</div>
+            <div className='takeprofit'>TP:156.676</div>
+            <div className='stoploss'>SL: 132.378</div>
+            <div className='profit'>PNL: -120.00</div>
+          </div>
+          <div className='logdetails'>
+            <div className='date'>25 March 2024</div>
+            <div className='pastpair'>EUR/USD</div>
+            <div className='pastposition'>BUY</div>
+            <div className='takeprofit'>TP:156.676</div>
+            <div className='stoploss'>SL: 132.378</div>
+            <div className='profit'>PNL: -120.00</div>
+          </div>
+        </div>
+        <div className='rankcont'>
+          <div className='contname'>Rankings</div>
+          <div className='ranksec'>
+            <div className='rankdet'>
+              <div className='rankno'>1</div>
+              <img src={profimg}/>
+              <div className='name'>Molly Whitaker</div>
+              <div className='tradesnums'>Trades Made: 56</div>
+              <div className='winrate'>Win Rate: 90%</div>
+              <div className='capital'>$ 50000</div>
+            </div>
+            <div className='rankdet'>
+              <div className='rankno'>1</div>
+              <img src={profimg}/>
+              <div className='name'>Molly Whitaker</div>
+              <div className='tradesnums'>Trades Made: 56</div>
+              <div className='winrate'>Win Rate: 90%</div>
+              <div className='capital'>$ 50000</div>
+            </div>
+            <div className='rankdet'>
+              <div className='rankno'>1</div>
+              <img src={profimg}/>
+              <div className='name'>Molly Whitaker</div>
+              <div className='tradesnums'>Trades Made: 56</div>
+              <div className='winrate'>Win Rate: 90%</div>
+              <div className='capital'>$ 50000</div>
+            </div>
+            <div className='rankdet'>
+              <div className='rankno'>1</div>
+              <img src={profimg}/>
+              <div className='name'>Whitaker</div>
+              <div className='tradesnums'>Trades Made: 56</div>
+              <div className='winrate'>Win Rate: 90%</div>
+              <div className='capital'>$ 50000</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
