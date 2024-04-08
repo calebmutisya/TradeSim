1a1b9d6a2ef46123e4736fee1833fffa397c123f
calebmutisya <mutisyacalebmusau@gmail.com>
2024-04-08 12:27:53 +0300

    Navbar responsive

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { NavLink } from 'react-router-dom'
 import '../css/Navbar.css';
 import logo from '../assets/Logo.svg'
+import hamburgermenu from '../assets/menu.svg'
+import xmark from '../assets/cross.svg'
+
 export default function Navbar() {
+
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+
+  const showSidebar = () => {
+    setSidebarVisible(true);
+  };
+
+  const hideSidebar = () => {
+    setSidebarVisible(false);
+  };
+
   return (
     <nav>
-      <div className='logo'><img src={logo}/>TRADESIM</div>
+      <div className='logo'>
+        <img src={hamburgermenu} onClick={showSidebar} className='menuicon'/>
+        <img className='tlogo' src={logo}/>TRADESIM
+      </div>
+      <div className={sidebarVisible ? 'ulinks visible' : 'ulinks'}>
+        <img className='xicon' onClick={hideSidebar} src={xmark}/>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/markets">Markets</NavLink>
+        <NavLink to="/news">News</NavLink>
+        <NavLink to="/analysis">Analysis</NavLink>
+        <NavLink to="/guide">Guide</NavLink>
+        <NavLink to="/about">About Us</NavLink>
+      </div>
       <div className='links'>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/markets">Markets</NavLink>
