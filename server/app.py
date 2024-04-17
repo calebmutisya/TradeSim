b83ad5eaad368f893b1653d6fd093b92e1116981
calebmutisya <mutisyacalebmusau@gmail.com>
2024-04-17 14:21:12 +0300

    imported tables

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from models import db,User,Opentrades,Closedtrades
 
 from views import *
 
