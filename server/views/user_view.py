d3be75a5b51ba3523927636ee1864c310a721dca
calebmutisya <mutisyacalebmusau@gmail.com>
2024-04-18 13:19:52 +0300

    user added succesfully

--- server/views/user_view.py
+++ server/views/user_view.py
@@ -11,3 +11,24 @@ def get_users():
     users = [user.to_dict() for user in User.query.order_by(User.capital.desc()).all()]
     return jsonify(users), 200
 
+# add user
+@user_bp.route("/addusers", methods=["POST"])
+def add_users():
+    data = request.get_json()
+    username = data['username']
+    email = data['email']
+    password = data['password']
+
+    check_username = User.query.filter_by(username=username).first()
+    check_email = User.query.filter_by(email=email).first()
+    
+
+    if check_username or check_email :
+        return jsonify({"error": "User email/username/phone already exist!"})
+
+    else:
+        new_user = User(email=email, password=password, username=username)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"success": "User added successfully!"}), 201
+
