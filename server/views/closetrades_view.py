66125140ff1f360941596343e7f7df5c76b2cdf4
calebmutisya <mutisyacalebmusau@gmail.com>
2024-04-18 11:29:55 +0300

    Close trade crud coded

--- server/views/closetrades_view.py
+++ server/views/closetrades_view.py
@@ -1,3 +1,12 @@
 from flask import request, jsonify, Blueprint
+from models import db, User,Opentrades,Closedtrades
+
 
 close_bp = Blueprint('close_bp', __name__)
+
+#get all closed trades for a user filter by showing the latest trade first
+@close_bp.route("/closedtrades/<int:user_id>", methods=["GET"])
+def get_all(user_id):
+    closed_trades = Closedtrades.query.filter_by(user_id=user_id).order_by(Closedtrades.open_date.desc()).all()
+    return jsonify([trade.serialize() for trade in closed_trades]), 200
+
