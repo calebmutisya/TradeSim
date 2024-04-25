4b422c7843e9e7677d1602b71e5b00b67221d36b
calebmutisya <mutisyacalebmusau@gmail.com>
2024-04-25 14:54:20 +0300

    Added jwt

--- server/views/closetrades_view.py
+++ server/views/closetrades_view.py
@@ -1,18 +1,23 @@
 from flask import request, jsonify, Blueprint
 from models import db, User,Opentrades,Closedtrades
+from flask_jwt_extended import  jwt_required, get_jwt_identity
+
 
 
 close_bp = Blueprint('close_bp', __name__)
 
 #get all closed trades for a user filter by showing the latest trade first
-@close_bp.route("/closedtrades/<int:user_id>", methods=["GET"])
+@close_bp.route("/closedtrades", methods=["GET"])
+@jwt_required()
 def get_all_closed_trades(user_id):
-    user = User.query.get(user_id)
+    current_user_id = get_jwt_identity()
+
+    user = User.query.get(current_user_id)
 
     if not user:
         return jsonify({"error": "User not found"}), 404
 
-    closed_trades = Closedtrades.query.filter_by(user_id=user_id).order_by(Closedtrades.open_date.desc()).all()
+    closed_trades = Closedtrades.query.filter_by(user_id=current_user_id).order_by(Closedtrades.open_date.desc()).all()
 
     if not closed_trades:
         return jsonify({"message": "No closed trades for this user"}), 200
