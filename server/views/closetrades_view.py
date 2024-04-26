e6e62b08d10246f8e9f4c222256d5f8a0d27beca
calebmutisya <mutisyacalebmusau@gmail.com>
2024-04-26 11:12:05 +0300

    Crud working with jwt

--- server/views/closetrades_view.py
+++ server/views/closetrades_view.py
@@ -1,15 +1,37 @@
 from flask import request, jsonify, Blueprint
-from models import db, User,Opentrades,Closedtrades
+from models import db, User,Closedtrades
 from flask_jwt_extended import  jwt_required, get_jwt_identity
-
 
 
 close_bp = Blueprint('close_bp', __name__)
 
+#add a closed trade
+@close_bp.route("/closedtrades", methods=["POST"])
+@jwt_required()
+def add_closedtrade():
+    current_user_id = get_jwt_identity()
+
+    data = request.json
+    new_closedtrade = Closedtrades(
+        user_id=current_user_id,
+        currency_pair=data.get('currency_pair'),
+        position=data.get('position'),
+        tp=data.get('tp'),
+        ep=data.get('ep'),
+        sl=data.get('sl'),
+        lot=data.get('lot'),
+        pnl=data.get('pnl'),
+        open_date=data.get('open_date')
+    )
+    db.session.add(new_closedtrade)
+    db.session.commit()
+    return jsonify({"message": "Trade added successfully", "id": new_closedtrade.id}), 201
+
+
 #get all closed trades for a user filter by showing the latest trade first
 @close_bp.route("/closedtrades", methods=["GET"])
 @jwt_required()
-def get_all_closed_trades(user_id):
+def get_all_closed_trades():
     current_user_id = get_jwt_identity()
 
     user = User.query.get(current_user_id)
