de67e4ef03157991c49f54951404123811923b4e
calebmutisya <mutisyacalebmusau@gmail.com>
2024-04-30 12:33:49 +0300

    added user provider

--- client/src/App.js
+++ client/src/App.js
@@ -9,23 +9,26 @@ import Guide from './pages/Guide.js'
 import Article from './pages/Article.js'
 import About from './pages/About.js'
 import Profile from './pages/Profile.js'
+import UserProvider from './context/UserContext.js';
 
 
 function App() {
   return (
       <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home/>}/>
-            <Route path="/markets" element={<Markets/>}/>
-            <Route path="/news" element={<News/>}/>
-            <Route path="/analysis" element={<Analysis/>}/>
-            <Route path="/guide" element={<Guide/>}/>
-            <Route path="/article" element={<Article/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/profile" element={<Profile/>}/>
-          </Route>
-        </Routes>
+        <UserProvider>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home/>}/>
+              <Route path="/markets" element={<Markets/>}/>
+              <Route path="/news" element={<News/>}/>
+              <Route path="/analysis" element={<Analysis/>}/>
+              <Route path="/guide" element={<Guide/>}/>
+              <Route path="/article" element={<Article/>}/>
+              <Route path="/about" element={<About/>}/>
+              <Route path="/profile" element={<Profile/>}/>
+            </Route>
+          </Routes>
+        </UserProvider>
       </Router>
   );
 }
