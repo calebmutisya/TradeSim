a9ed523c31877f55728a9b357c60685f3f68284a
calebmutisya <mutisyacalebmusau@gmail.com>
2024-05-24 16:35:04 +0300

    optimizing 3

--- server/install_chrome.sh
+++ server/install_chrome.sh
@@ -9,11 +9,10 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb || apt-get -fy install
 
 # Install ChromeDriver
-CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
-wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-chmod +x chromedriver
-mv -f chromedriver /usr/local/bin/chromedriver
+CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip -d /usr/local/bin/
+chmod +x /usr/local/bin/chromedriver
 
 # Clean up
 rm google-chrome-stable_current_amd64.deb
